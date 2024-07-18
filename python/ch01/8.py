5a919c569da8d9d079c40422093ad6bbe5ea08c5
pkd98 <baum12345@naver.com>
2024-07-18 23:40:50 +0900

    Add: python basic

---
+++ python/ch01/8.py
@@ -0,0 +1,16 @@
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+
+for i in range(10):
+    if i % 2 == 0:
+        continue
+    print(i)
+
+for i in range(10):
+    if i % 2 == 0:
+        pass
+    else:
+        print(i)
